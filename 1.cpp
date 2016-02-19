1a02ca0b6acef7dc0390d3e92f016a2584e429e4
Pulkit Sharma <avastpulkit@gmail.com>
2016-02-19 16:07:00 +0530

    Server Class implemented

--- 1.cpp
+++ 1.cpp
@@ -2,6 +2,38 @@
 
 using namespace std;
 
+double chances()
+{
+    return rand()/RAND_MAX;
+}
+
+class Server
+{
+    double success_rate;
+
+    Server(int rate):success_rate(rate)
+    {
+
+    }
+
+    Server()
+    {
+        success_rate = chances();
+    }
+
+    int request()
+    {
+        if(success_rate >= chances())
+        {
+            return 200;
+        }
+        else
+        {
+            return 400;
+        }
+    }
+};
+
 int main()
 {
 
