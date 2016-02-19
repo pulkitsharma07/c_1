37e1c96dceffb8f155d472891aa9d7f4cb8afe43
Pulkit Sharma <avastpulkit@gmail.com>
2016-02-19 16:59:30 +0530

    Implemented Batch requests

--- 1.cpp
+++ 1.cpp
@@ -9,6 +9,7 @@ double chances()
 
 struct server
 {
+
     double success_rate;
 
     server(double rate)
@@ -34,16 +35,124 @@ struct server
     }
 };
 
+struct client
+{
+    vector<server> servers;
+    int *priorities;
+    int requests_to_success;
 
+    client(int num_servers)
+    {
+        priorities = new int[num_servers];
+
+        for(int i = 0 ; i < num_servers ; i++)
+        {
+            servers.push_back(server(chances()));
+        }
+
+    }
+
+    client()
+    {
+        servers.resize(3);
+
+        servers[0].success_rate = 0.9;
+        servers[1].success_rate = 0.8;
+        servers[2].success_rate = 0.7;
+    }
+
+    int request_key(int server_id)
+    {
+       int server_response = servers[server_id].request();
+       return server_response;
+    }
+
+
+    int acknowledge(int server_id)
+    {
+
+    }
+
+    int request(int server_id)
+    {
+        int status = request_key(server_id);
+
+        if(status == 200)
+        {
+            acknowledge(server_id);
+        }
+        else
+        {
+
+        }
+
+        return status;
+    }
+
+
+    int initiate_req()
+    {
+        int requests_to_success = 1;
+        bool success = false;
+
+
+        for(int i = 0 ;i < servers.size();i++)
+        {
+             int response = request(i);
+
+            //cout<<"Requesting server :"<<i<<" "<<"status code :"<<response<<endl;
+
+            if(response == 200)
+            {
+                success = true;
+                break;
+            }
+            else
+            {
+               // cout<<"Request FAILED";
+                requests_to_success ++;
+            }
+
+          //  system("pause");
+        }
+
+        if(!success) //
+        {
+            cout<<"\n\n NO SUCCESS ENCOUNTERED";
+        }
+        else
+        {
+            cout<<"Success after "<<requests_to_success<<" requests";
+        }
+
+        return requests_to_success;
+    }
+
+
+    void batch_request(int num)
+    {
+        int total_requests = 0;
+
+        while(num--)
+        {
+           int reqs = initiate_req();
+           total_requests += reqs;
+        }
+
+        cout<<"Had to make "<<total_requests<<" requests";
+    }
+
+};
 int main()
 {
-    server a(0.3);
+    srand(time(NULL));
+
+    client c;
 
 
-    while(1)
-    {
-        cout<<a.request()<<endl;
-        system("PAUSE");
-    }
+    c.initiate_req();
+
+    c.batch_request(10000);
+
     return 0;
 }
